592b00167539781c119d164ebfe9ad1ce491cc33
AndreuFerrandis <153548161+AndreuFerrandis@users.noreply.github.com>
2024-02-19 20:26:14 +0000

    acabado

--- src/js/app.js
+++ src/js/app.js
@@ -33,9 +33,9 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>${variables.name}</h1>
+          <h1>${variables.name} ${variables.lastName}</h1>
           <h2>${variables.role}</h2>
-          <h3>${variables.city}</h3>
+          <h3>${variables.city}, ${variables.country}</h3>
           <ul class="${variables.socialMediaPosition}">
             <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
             <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
